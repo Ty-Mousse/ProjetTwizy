755c2223adf7f0aa3e86f7ed388990da3fb73923
Clément Rault <clem.rault@outlook.com>
2022-03-28 09:56:56 +0200

    Setup window

--- src/main/java/display/Window.java
+++ src/main/java/display/Window.java
@@ -1,56 +1,73 @@
 package display;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 public class Window {
 
+    private final ImageIcon icon = new ImageIcon("./src/main/resources/logo.png");
     private final JFrame window;
     private File currentFile;
+    private final JLabel labelText = new JLabel("No file selected...");
+    private final TextArea log = new TextArea();
 
     public Window (String title, int width, int height) {
         this.window = new JFrame();
         this.window.setTitle(title);
         this.window.setSize(width, height);
         this.window.setResizable(false);
+        this.window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.window.setIconImage(icon.getImage());
+        this.window.setContentPane(this.initWindow());
         this.window.setVisible(true);
-        this.initWindow();
     }
 
-    private void initWindow() {
+    private JPanel initWindow() {
+        // Ajout d'un panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
         // Ajout du bouton d'upload de fichier
-        JButton button = new JButton("Upload a file");
-        button.addActionListener(new ActionListener() {
+        JButton upload = new JButton("Upload");
+        upload.setPreferredSize(new Dimension(100, 20));
+        upload.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 currentFile = openFileChooser();
-                try {
-                    updateRender();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+                showImage(currentFile.getName(), currentFile);
+                updateRender();
             }
         });
-        this.window.add(button, BorderLayout.PAGE_START);
+        panel.add(upload, BorderLayout.PAGE_START);
+
+        // Ajout du label
+        panel.add(labelText);
+
+        // Ajout du bouton pour reconnaitre les panneaux
+        JButton solve = new JButton("Solve");
+        solve.setPreferredSize(new Dimension(100, 20));
+        panel.add(solve);
+
+        // Ajout du textarea de logs
+        log.setEditable(false);
+        panel.add(log);
+
+        return panel;
     }
 
-    private void updateRender() throws IOException {
-        BufferedImage myPicture = ImageIO.read(currentFile);
-        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-        this.window.add(picLabel);
-        this.window.pack();
+    private void updateRender() {
+        if (currentFile != null) {
+            labelText.setText(currentFile.getName());
+            log.append(currentFile.getName() + " as been uploaded.\n");
+        }
     }
 
     private File openFileChooser() {
         JFileChooser chooser = new JFileChooser("./");
-        FileNameExtensionFilter filter = new FileNameExtensionFilter(
-                "JPG, PNG or MP4 files", "jpg", "png", "mp4");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, PNG or MP4 files", "jpg", "png", "mp4");
         chooser.setFileFilter(filter);
         int returnVal = chooser.showOpenDialog(this.window);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
@@ -59,15 +76,24 @@ public class Window {
         return chooser.getSelectedFile();
     }
 
+    private void showImage(String title, File img) {
+        try {
+            JFrame frame = new JFrame();
+            frame.setTitle(title);
+            frame.getContentPane().add(new JLabel(new ImageIcon(String.valueOf(img))));
+            frame.setIconImage(icon.getImage());
+            frame.pack();
+            frame.setVisible(true);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public JFrame getWindow() {
         return window;
     }
 
     public File getCurrentFile() {
         return currentFile;
-    }
-
-    public void setCurrentFile(File currentFile) {
-        this.currentFile = currentFile;
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("mat = " + mat.dump());
-        Window window = new Window("Logiciel OpenCV", 720, 480);
+        Window window = new Window("PanelFinder", 540, 240);
     }
 }
