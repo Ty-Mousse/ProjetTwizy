a87c98cdd06460604cc8fcdec32d11f736ccebb9
MiniiiB <thomas.bonnet46@hotmail.fr>
2022-04-07 09:21:36 +0200

    Ajout amélioration affichage panneau

---
+++ src/main/java/roadSignClarification/roadSignClarification.java
@@ -0,0 +1,25 @@
+package roadSignClarification;
+
+
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.List;
+
+public class roadSignClarification {
+
+    public static Mat improvedDisplay(Mat image, List<String> label, List<MatOfPoint> contours) {
+        MatOfPoint2f matOfPoint2f = new MatOfPoint2f();
+        float[] radius = new float[1];
+        Point center = new Point();
+        for (int c = 0; c < contours.size(); c++) {
+            MatOfPoint contour = contours.get(c);
+            matOfPoint2f.fromList(contour.toList());
+            Rect rect = Imgproc.boundingRect(contour);
+            Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
+            Core.putText(image, label.get(c), new Point(rect.x + rect.width + 20, rect.y + rect.height + 20), Core.FONT_HERSHEY_COMPLEX, 0.7, new Scalar(0, 255, 0), 2);
+        }
+        return image;
+    }
+
+}
